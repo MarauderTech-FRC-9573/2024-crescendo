5394dfa4be65756bb40ef4f526793d6e8e8e80d1
rake6682 <97271338+rake6682@users.noreply.github.com>
2024-01-12 21:09:11 -0800

    changed the drivetrain ports so the sim connects, idk how to configure sim tho

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -9,8 +9,8 @@ public class DriveSubsystem extends SubsystemBase {
     // aribitrary values for now, double check each of the channels 
     private Spark driveLeftMotor = new Spark(0);
     private Spark driveRightMotor = new Spark(1);
-    private Encoder driveLeftEncoder = new Encoder(0, 1);
-    private Encoder driveRightEncoder = new Encoder(2, 3);
+    private Encoder driveLeftEncoder = new Encoder(2, 3);
+    private Encoder driveRightEncoder = new Encoder(4, 5);
 
     private final double kEncoderTick2Meter = 1.0 / 4096.0 * 0.128 * Math.PI; // converting encoder ticks to meters
 
