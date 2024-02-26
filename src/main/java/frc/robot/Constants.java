a55df2e81fe833b521ff913904dfeba30d34b27c
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2024-02-26 15:19:02 -0800

    code for photonvision

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.plant.DCMotor;
@@ -85,4 +87,56 @@ public final class Constants {
   }
   public static final class OIConstants {    
   }
+
+  public static class VisionConstants {
+    public static final String kCameraName1 = "Camera1";
+    public static final String kCameraName2 = "Camera2";
+    public static final Transform3d kCamera1ToRobotOffset = new Transform3d();
+    public static final Transform3d kCamera2ToRobotOffset = new Transform3d(0.0, -0.12, 0.0, new Rotation3d());
+
+
+    public static final double kCamera1HeightMeters = 0.215;
+    public static final double kCamera2HeightMeters = 0.0;
+    public static final double kCamera3HeightMeters = 0.0;
+
+    public static final double KCameraPitchRadians = 0.0;
+
+    public static final double kTarget1HeightMeters = 0.540;
+    public static final double kTarget2HeightMeters = 0.522;
+    public static final double kTarget3HeightMeters = 0.530;
+    public static final double kTarget4HeightMeters = 0.0;
+    public static final double kTarget5HeightMeters = 0.0;
+    public static final double kTarget6HeightMeters = 0.0;
+    public static final double kTarget7HeightMeters = 0.0;
+    public static final double kTarget8HeightMeters = 0.0;
+
+    public static double findTargetHeight(int targetID) {
+      switch(targetID) {
+        case 1: targetID = 1;
+          return kTarget1HeightMeters;
+        case 2: targetID = 2;
+          return kTarget2HeightMeters;
+        case 3: targetID = 3;
+          return kTarget3HeightMeters;
+        case 4: targetID = 4;
+          return kTarget4HeightMeters;
+        case 5: targetID = 5;
+          return kTarget5HeightMeters;
+        case 6: targetID = 6;
+          return kTarget6HeightMeters;
+        case 7: targetID = 7;
+          return kTarget7HeightMeters;
+        case 8: targetID = 8;
+          return kTarget8HeightMeters;
+        default: throw new Error("Invalid Target ID");
+      }
+    }
+  }
+
+  public static class FieldConstants {
+    public static final double kFieldLength = 4.0;
+    public static final double kFieldWidth = 3.0;
+  }
+
+
 }
