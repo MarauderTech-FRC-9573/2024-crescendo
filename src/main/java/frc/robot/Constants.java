2730533ca967e65b72ca96e5d752458bd378889e
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2024-02-26 14:55:42 -0800

    boilerplate code for photon vision
    https://github.com/TeraViks/PhotonVision/blob/main/src/main/java/frc/robot/subsystems/CameraSubsystem.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,6 +16,11 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static class FieldConstants {
+    public static final double kFieldLength = 4.0;
+    public static final double kFieldWidth = 3.0;
+  }
+  
   public static final class DriveConstants {
     public static final int kLeftMotorPort = 0;
     public static final int kRightMotorPort = 0;
@@ -25,6 +33,52 @@ public final class Constants {
   public static final class ShooterConstants {
 
   }
+
+  public static final class VisionConstants {
+    public static final String kCameraName1 = "Camera1";
+    public static final String kCameraName2 = "Camera2";
+    public static final Transform3d kCamera1ToRobotOffset = new Transform3d();
+    public static final Transform3d kCamera2ToRobotOffset = new Transform3d(0.0, -0.12, 0.0, new Rotation3d());
+
+
+    public static final double kCamera1HeightMeters = 0.215;
+    public static final double kCamera2HeightMeters = 0.0;
+    public static final double kCamera3HeightMeters = 0.0;
+
+    public static final double KCameraPitchRadians = 0.0;
+
+    public static final double kTarget1HeightMeters = 0.540;
+    public static final double kTarget2HeightMeters = 0.522;
+    public static final double kTarget3HeightMeters = 0.530;
+    public static final double kTarget4HeightMeters = 0.0;
+    public static final double kTarget5HeightMeters = 0.0;
+    public static final double kTarget6HeightMeters = 0.0;
+    public static final double kTarget7HeightMeters = 0.0;
+    public static final double kTarget8HeightMeters = 0.0;
+
+    public static double findTargetHeight(int targetID) {
+      switch(targetID) {
+        case 1: targetID = 1;
+          return kTarget1HeightMeters;
+        case 2: targetID = 2;
+          return kTarget2HeightMeters;
+        case 3: targetID = 3;
+          return kTarget3HeightMeters;
+        case 4: targetID = 4;
+          return kTarget4HeightMeters;
+        case 5: targetID = 5;
+          return kTarget5HeightMeters;
+        case 6: targetID = 6;
+          return kTarget6HeightMeters;
+        case 7: targetID = 7;
+          return kTarget7HeightMeters;
+        case 8: targetID = 8;
+          return kTarget8HeightMeters;
+        default: throw new Error("Invalid Target ID");
+      }
+    }
+  }
+
 
   public static final class OIConstants {
     public static final int kDriverJoystickPort = 0;
--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,60 +1,152 @@
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.PhotonPoseEstimator;
+
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.CvSink;
-import edu.wpi.first.cscore.CvSource;
-import edu.wpi.first.cscore.UsbCamera;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
+import frc.robot.Constants.VisionConstants;
+import frc.robot.Constants.FieldConstants;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionSubsystem extends SubsystemBase {
-  private final UsbCamera camera;
-  private final CvSink cvSink;
-  private final CvSource outputStream;
+  String Camera1Name = "bob";
+  String Camera2Name = "john";
+  private final double kAprilTagOffsetMeters = 0.0;
+
+  PhotonCamera m_camera1;
+  PhotonCamera m_camera2;
+  PhotonPoseEstimator m_photonPoseEstimatorCam1;
+  PhotonPoseEstimator m_photonPoseEstimatorCam2;
+    PhotonPipelineResult m_resultCam1;
+  PhotonPipelineResult m_resultCam2;
+  Optional<PhotonTrackedTarget> m_lowestAmbiguityTarget;
+  AprilTag aprilTag1 = new AprilTag(1, new Pose3d(new Translation3d(4.0, 0.5+kAprilTagOffsetMeters, VisionConstants.kTarget1HeightMeters), new Rotation3d(0.0, 0.0, Math.PI)));
+  AprilTag aprilTag2 = new AprilTag(2, new Pose3d(new Translation3d(4.0, 2.5+kAprilTagOffsetMeters, VisionConstants.kTarget2HeightMeters), new Rotation3d(0.0, 0.0, Math.PI)));
+  AprilTag aprilTag3 = new AprilTag(3, new Pose3d(new Translation3d(4.0, 1.5+kAprilTagOffsetMeters, VisionConstants.kTarget3HeightMeters), new Rotation3d(0.0, 0.0, Math.PI)));
+  List<AprilTag> aprilTagList = Arrays.asList(aprilTag1, aprilTag2, aprilTag3);
+
+  AprilTagFieldLayout m_aprilTagFieldLayout = 
+  new AprilTagFieldLayout(
+    aprilTagList, FieldConstants.kFieldLength, FieldConstants.kFieldWidth
+  );
+
 
   public VisionSubsystem() {
-    camera = CameraServer.startAutomaticCapture();
-    camera.setResolution(640, 480);
+    m_camera1 = new PhotonCamera(Camera1Name);
+    m_camera2 = new PhotonCamera(Camera2Name);
 
-    cvSink = CameraServer.getVideo();
-    outputStream = CameraServer.putVideo("Rectangle", 640, 480);
-
-    // Start the vision thread
-    Thread visionThread = new Thread(this::visionProcessing);
-    visionThread.start();
+    m_photonPoseEstimatorCam1 = 
+    new PhotonPoseEstimator(
+      m_aprilTagFieldLayout,
+      PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+      m_camera1,
+      VisionConstants.kCamera1ToRobotOffset
+    );
+    
+    m_photonPoseEstimatorCam2 =
+    new PhotonPoseEstimator(
+      m_aprilTagFieldLayout,
+      PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+      m_camera2,
+      VisionConstants.kCamera2ToRobotOffset
+    );
   }
 
-  private void visionProcessing() {
-    Mat mat = new Mat();
+    public PoseStrategy getEstimatorStrategy() {
+    return m_photonPoseEstimatorCam1.getPrimaryStrategy();
+  }
 
-    while (!Thread.interrupted()) {
-      if (cvSink.grabFrame(mat) == 0) {
-        outputStream.notifyError(cvSink.getError());
+  public boolean hasTargetsCam1() {
+    return m_resultCam1.hasTargets();
+  }
+
+  public boolean hasTargetsCam2() {
+    return m_resultCam2.hasTargets();
+  }
+
+  public List<PhotonTrackedTarget> getAllTargets() {
+    List<PhotonTrackedTarget> combinedList = m_resultCam1.getTargets();
+    combinedList.addAll(m_resultCam2.getTargets());
+    return combinedList;
+  }
+
+  public void takeSnapshotCam1() {
+    m_camera1.takeInputSnapshot();
+  }
+
+  public void takeSnapshotCam2() {
+    m_camera2.takeInputSnapshot();
+  }
+
+  public ArrayList<Optional<EstimatedRobotPose>> getFieldRelativePoseEstimators() {
+    ArrayList<Optional<EstimatedRobotPose>> estimatorList = new ArrayList<Optional<EstimatedRobotPose>>();
+    estimatorList.add(m_photonPoseEstimatorCam1.update(m_resultCam1));
+    estimatorList.add(m_photonPoseEstimatorCam2.update(m_resultCam2));
+    return estimatorList;
+  }
+
+  public double getDistanceToTarget(PhotonTrackedTarget target) {
+    return (
+      PhotonUtils.calculateDistanceToTargetMeters(
+        VisionConstants.kCamera1HeightMeters,
+        VisionConstants.findTargetHeight(target.getFiducialId()),
+        VisionConstants.KCameraPitchRadians,
+        Units.degreesToRadians(target.getPitch())
+      )
+    );
+  }
+
+  public Pose3d getFieldRelativePose(PhotonTrackedTarget target) {
+    return PhotonUtils.estimateFieldToRobotAprilTag(
+        target.getBestCameraToTarget(),
+        m_aprilTagFieldLayout.getTagPose(target.getFiducialId()).get(),
+        VisionConstants.kCamera1ToRobotOffset
+      );
+  }
+
+  private Optional<PhotonTrackedTarget> getLowestAmbiguityTargetImpl(List<PhotonTrackedTarget> targetList) {
+    Optional<PhotonTrackedTarget> bestTarget = Optional.empty();
+    for (int i=0; i<targetList.size(); i++) {
+      PhotonTrackedTarget currentTarget = targetList.get(i);
+      if (currentTarget.getPoseAmbiguity() == -1 || currentTarget.getPoseAmbiguity() >= 0.2) continue;
+      if (bestTarget.isEmpty()) {
+        bestTarget = Optional.of(currentTarget);
         continue;
       }
-
-      // Perform your vision processing here
-      processImage(mat);
-
-      outputStream.putFrame(mat);
+      if (currentTarget.getPoseAmbiguity() < bestTarget.get().getPoseAmbiguity()) {
+        bestTarget = Optional.of(currentTarget);
+      }
     }
+    return bestTarget;
   }
 
-  private void processImage(Mat mat) {
-    // Example: Draw a rectangle on the image
-    Imgproc.rectangle(mat, new Point(100, 100), new Point(400, 400),
-        new Scalar(255, 255, 255), 5);
-
-    // Add your actual vision processing logic here
-    // You can use OpenCV functions to analyze and manipulate the image
-    // For example, thresholding, contour detection, etc.
+  public Optional<PhotonTrackedTarget> getLowestAmbiguityTarget() {
+    return m_lowestAmbiguityTarget;
   }
+
+
 
   @Override
   public void periodic() {
-    // Add any periodic tasks related to the vision subsystem here
+    m_resultCam1 = m_camera1.getLatestResult();
+    m_resultCam2 = m_camera2.getLatestResult();
+    m_lowestAmbiguityTarget = getLowestAmbiguityTargetImpl(getAllTargets());
   }
 }
