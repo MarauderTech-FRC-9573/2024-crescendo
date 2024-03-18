0ec344ad3f80bf46106a924864dce72ecb45909a
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2024-03-18 14:50:13 -0700

    resolve merge conflict

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,9 +2,51 @@ package frc.robot.subsystems;
 import frc.robot.Constants.ShooterConstants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class IntakeSubsystem extends SubsystemBase{
-    public IntakeSubsystem() {}
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel;
+import frc.robot.Constants.IntakeConstants;
+import edu.wpi.first.wpilibj2.command.*;
 
-    @Override
-    public void periodic() {}
-}
+//Almost same code as the shootersubsystem.
+public class IntakeSubsystem extends SubsystemBase {
+  CANSparkMax m_brushMotor;
+  CANSparkMax m_intakeMotor;
+  Boolean intakeMotor;
+  
+  public IntakeSubsystem() {
+    m_brushMotor = new CANSparkMax(IntakeConstants.brushMotorPort, CANSparkLowLevel.MotorType.kBrushless);
+    m_intakeMotor = new CANSparkMax(IntakeConstants.intakeMotorPort, CANSparkLowLevel.MotorType.kBrushless);
+    intakeMotor = true;
+  }
+    
+    public void setBrushMotor(double speed) {
+      m_brushMotor.set(speed);
+    }
+    
+    
+    public void setIntakeMotor(double speed) {
+      m_intakeMotor.set(speed);
+    }
+    
+    public void stop() {
+      m_brushMotor.set(0);
+      m_intakeMotor.set(0);
+    }
+    
+    public Command moveIntake() {
+      return new SequentialCommandGroup(
+        new InstantCommand(() -> {
+      if (intakeMotor) {
+        setIntakeMotor(-IntakeConstants.IntakeMotorMoveBack);
+      } else {
+        setIntakeMotor(-IntakeConstants.IntakeMotorMoveForward);
+      }
+      
+      intakeMotor = !intakeMotor; 
+      }, this),
+      new WaitCommand(1),
+      new InstantCommand(this::stop, this)
+      );      
+      
+    }
+  }
