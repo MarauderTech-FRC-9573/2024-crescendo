664980b432a39cd73b75ec0f8bafc48b1e163ae9
Rm417 PC1 <>
2024-03-20 20:13:37 -0700

    removing references to PID, sysID

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,13 +3,9 @@ package frc.robot;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.IntakeArmSwitch;
 import frc.robot.commands.LaunchAmp;
 import frc.robot.commands.LaunchSpeaker;
@@ -25,73 +21,43 @@ import frc.robot.subsystems.VisionSubsystem;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.ShooterConstants;
 import frc.robot.commands.Autos;
-import frc.robot.commands.DriveForwardCmd;
 
 public class RobotContainer {
   private final DriveSubsystem driveSubsystem = new DriveSubsystem();
   private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
   private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
   private final VisionSubsystem visionSubsystem = new VisionSubsystem();
-  
+
   private final CommandXboxController driveController = new CommandXboxController(DriveConstants.driveControllerPort);
   private final CommandXboxController operatorController = new CommandXboxController(DriveConstants.operatorControllerPort);
-  
+
   private final PowerDistribution pdh = new PowerDistribution(1, ModuleType.kRev);
-  
-  private final SendableChooser<Command> m_autoChooser = new SendableChooser<Command>();
-  
   
   public RobotContainer() {
     pdh.setSwitchableChannel(true);
     configureButtonBindings();
-    SmartDashboard.putData(m_autoChooser);
-  }
-  
-  public void initializeSubsystems() { 
     driveSubsystem.setDefaultCommand(new RunCommand(() -> driveSubsystem.arcadeDrive(-driveController.getLeftY(), -driveController.getRightX()), driveSubsystem));
-    intakeSubsystem.setDefaultCommand(new RunCommand(() -> intakeSubsystem.runArm(() -> driveController.getLeftY())));
-    SmartDashboard.putData(intakeSubsystem);
-  }
-  
-  public void initializeAutoChooser() {
-    m_autoChooser.setDefaultOption("Drive forward: ",
-    new WaitCommand(0.1).andThen(new DriveForwardCmd(driveSubsystem, 10, 0.5)).withTimeout(1).andThen(new RunCommand(() -> driveSubsystem.arcadeDrive(0, 0), driveSubsystem)));
-    
-    m_autoChooser.addOption("SysID Quasistatic Forward: ",
-    intakeSubsystem.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    
-    m_autoChooser.addOption("SysId Quasistatic Backward: ", intakeSubsystem.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    
-    m_autoChooser.addOption("SysID Dynamic Forward: ", intakeSubsystem.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    
-    m_autoChooser.addOption("SysID Dynamic Backward: ", intakeSubsystem.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-    
   }
   
   private void configureButtonBindings() {
     /*Create an inline sequence to run when the operator presses and holds the A (green) button. Run the PrepareLaunch
-    * command for 1 seconds and then run the LaunchNote command */
-    // operatorController.a().whileTrue(new PrepareLaunchSpeaker(shooterSubsystem).withTimeout(1).andThen(new LaunchSpeaker(shooterSubsystem)).handleInterrupt(() -> shooterSubsystem.stop()));
-    // operatorController.b().whileTrue(new PrepareLaunchAmp(shooterSubsystem).withTimeout(1).andThen(new LaunchAmp(shooterSubsystem)).handleInterrupt(() -> shooterSubsystem.stop()));
-    
+     * command for 1 seconds and then run the LaunchNote command */
+    operatorController.a().whileTrue(new PrepareLaunchSpeaker(shooterSubsystem).withTimeout(1).andThen(new LaunchSpeaker(shooterSubsystem)).handleInterrupt(() -> shooterSubsystem.stop()));
+    operatorController.b().whileTrue(new PrepareLaunchAmp(shooterSubsystem).withTimeout(1).andThen(new LaunchAmp(shooterSubsystem)).handleInterrupt(() -> shooterSubsystem.stop()));
+
     // Set up a binding to run the intake command while the operator is pressing and holding the left Bumper
     operatorController.leftBumper().whileTrue(shooterSubsystem.getIntakeCommand());
     
     // Ground Intake Button Bindings
     operatorController.rightBumper().whileTrue(new IntakeArmSwitch(intakeSubsystem).handleInterrupt(() -> intakeSubsystem.stop()));
-    // operatorController.x().whileTrue(new IntakeReceiver(intakeSubsystem).handleInterrupt(() -> intakeSubsystem.stop()));
-    // operatorController.y().whileTrue(new IntakeReleaser(intakeSubsystem).withTimeout(1).handleInterrupt(() -> intakeSubsystem.stop()));
-    
-    operatorController.x().whileTrue(intakeSubsystem.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    operatorController.y().whileTrue(intakeSubsystem.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    operatorController.a().whileTrue(intakeSubsystem.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    operatorController.b().whileTrue(intakeSubsystem.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-    
-    
+    operatorController.x().whileTrue(new IntakeReceiver(intakeSubsystem).handleInterrupt(() -> intakeSubsystem.stop()));
+    operatorController.y().whileTrue(new IntakeReleaser(intakeSubsystem).withTimeout(1).handleInterrupt(() -> intakeSubsystem.stop()));
+  
   }
   
   public Command getAutonomousCommand() {
-    return m_autoChooser.getSelected();
+    return Autos.exampleAuto(driveSubsystem, driveController);
+    }
   }
-}
-
+  
+  
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,114 +1,49 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.MetersPerSecond;
-import static edu.wpi.first.units.Units.Volts;
 import static frc.robot.Constants.ShooterConstants;
-
-import java.util.function.DoubleSupplier;
-
-import static edu.wpi.first.units.MutableMeasure.mutable;
-import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.MetersPerSecond;
-import static edu.wpi.first.units.Units.Volts;
-import edu.wpi.first.units.MutableMeasure;
-
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel;
 import frc.robot.Constants.IntakeConstants;
-import frc.robot.Constants.ShooterConstants;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.units.Distance;
-import edu.wpi.first.units.Measure;
-import edu.wpi.first.units.MutableMeasure;
-import edu.wpi.first.units.Velocity;
-import edu.wpi.first.units.Voltage;
-import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.*;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 
 //Almost same code as the shootersubsystem.
 public class IntakeSubsystem extends SubsystemBase {
-  CANSparkMax m_IntakeMotor = new CANSparkMax(IntakeConstants.IntakeMotorPort, CANSparkLowLevel.MotorType.kBrushless);
-  CANSparkMax m_ArmMotor = new CANSparkMax(IntakeConstants.ArmMotorPort, CANSparkLowLevel.MotorType.kBrushless);
+  CANSparkMax m_IntakeMotor;
+  CANSparkMax m_ArmMotor;
   Boolean intakeMotor;
   double positionArm;
   
-  // Mutable holder for unit-safe voltage values, persisted to avoid reallocation.
-  private final MutableMeasure<Voltage> m_appliedVoltage = mutable(Volts.of(0));
-  // Mutable holder for unit-safe linear distance values, persisted to avoid reallocation.
-  private final MutableMeasure<Distance> m_distance = mutable(Meters.of(0));
-  // Mutable holder for unit-safe linear velocity values, persisted to avoid reallocation.
-  private final MutableMeasure<Velocity<Distance>> m_velocity = mutable(MetersPerSecond.of(0));
-  public PIDController pid = new PIDController(IntakeConstants.kPArm, IntakeConstants.kIArm, IntakeConstants.kDArm);
-  private final SimpleMotorFeedforward m_shooterFeedforward =
-  new SimpleMotorFeedforward(
-  IntakeConstants.kSVolts,
-  IntakeConstants.kVVoltSecondsPerRotation,
-  IntakeConstants.kAVoltSecondsSquaredPerRotation);
-  
-  
-  
-  
-  private final SysIdRoutine m_sysIdRoutine = new SysIdRoutine(new SysIdRoutine.Config(), new SysIdRoutine.Mechanism((Measure<Voltage> volts) -> {
-    m_ArmMotor.setVoltage(volts.in(Volts));
-  },
-  log -> {
-    log.motor("arm-motor")
-    .voltage(m_appliedVoltage.mut_replace(m_ArmMotor.get() * RobotController.getBatteryVoltage(), Volts))
-    .linearPosition(m_distance.mut_replace(m_ArmMotor.getEncoder().getPosition(), Meters))
-    .linearVelocity(m_velocity.mut_replace(m_ArmMotor.getEncoder().getVelocity(), MetersPerSecond));
-  }, this));
-  
-  
-  
   public IntakeSubsystem() {
+    m_IntakeMotor = new CANSparkMax(IntakeConstants.IntakeMotorPort, CANSparkLowLevel.MotorType.kBrushless);
+    m_ArmMotor = new CANSparkMax(IntakeConstants.ArmMotorPort, CANSparkLowLevel.MotorType.kBrushless);
+  }
     
-  }
-  
-  public void setIntakeMotor(double speed) {
-    m_IntakeMotor.set(speed);
-  }
-  
-  public double getArmPostition() {
-    return positionArm;
-  }
-  
-  public void setArmMotor(double speed) {
-    m_ArmMotor.set(speed);
-  }
-  
-  public void setArmPosition(double position) {
-    m_ArmMotor.getEncoder().setPosition(0.0);
-  }
-  
-  public void stop() {
-    m_IntakeMotor.set(0);
-    m_ArmMotor.set(0);
-  }
+    public void setIntakeMotor(double speed) {
+      m_IntakeMotor.set(speed);
+    }
+    
+    public double getArmPostition() {
+      return positionArm;
+    }
+    
+    public void setArmMotor(double speed) {
+      m_ArmMotor.set(speed);
+    }
 
-  @Override
-  public void periodic() { 
-    positionArm = m_ArmMotor.getEncoder().getPosition();
-    System.out.println("Arm Motor Position: " + positionArm);
-  }
+    public void setArmPosition(double position) {
+      m_ArmMotor.getEncoder().setPosition(0.0);
+    }
+    
+    public void stop() {
+      m_IntakeMotor.set(0);
+      m_ArmMotor.set(0);
+    }
 
-  // SYSID Functions
-  public Command runArm(DoubleSupplier runSpeed) {
-    return new RunCommand(() -> {
-      m_ArmMotor.setVoltage(pid.calculate(m_ArmMotor.getEncoder().getVelocity(), runSpeed.getAsDouble()) + m_shooterFeedforward.calculate(runSpeed.getAsDouble()));
-    });
+    @Override
+    public void periodic() { 
+      positionArm = m_ArmMotor.getEncoder().getPosition();
+      System.out.println("Arm Motor Position: " + positionArm);
+    }
   }
-
-  public Command sysIdQuasistatic(SysIdRoutine.Direction direction) { 
-    return m_sysIdRoutine.quasistatic(direction);
-  } 
-
-  public Command sysIdDynamic(SysIdRoutine.Direction direction) {
-    return m_sysIdRoutine.dynamic(direction);
-  }
-}
