6170de444301b2cf77e5e922a933202c15e6d695
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2024-03-20 18:08:33 -0700

    fixexd missing import
    Co-Authored-By: Andy Ly <97271338+rake6682@users.noreply.github.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import org.littletonrobotics.urcl.URCL;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
