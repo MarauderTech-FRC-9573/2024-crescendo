8b3de96f0053ecf5c4571c84b4af4a62bc0e8b34
Rm417 PC1 <>
2024-03-21 18:17:21 -0700

    Revert "trying to add missing URCL logging stuff"
    This reverts commit f73f32dbac0846ce90f76497d74d646316cb0b60.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -31,8 +31,9 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    Logger.registerURCL(URCL.startExternal());
-    Logger.start();
+
+    //Log stuff hopefully
+
     DataLogManager.start();
     URCL.start();
     
