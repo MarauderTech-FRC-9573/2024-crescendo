d875662886e98eef4cc4560e1664dee5d214b32b
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2024-03-22 12:21:57 -0700

    delete unneeded controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,7 +20,6 @@ public class RobotContainer {
   private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
   private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
 
-  private final CommandXboxController driveController = new CommandXboxController(DriveConstants.driveControllerPort);
   private final CommandXboxController operatorController = new CommandXboxController(DriveConstants.operatorControllerPort);
 
   private final PowerDistribution pdh = new PowerDistribution(1, ModuleType.kRev);
